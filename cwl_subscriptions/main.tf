f5dc2a94ebab9cfc2d5cd30a38677c1a881a9881
Dustin Whited <dgwhited@gmail.com>
2022-10-21 12:47:00 -0500

    correct value

--- cwl_subscriptions/main.tf
+++ cwl_subscriptions/main.tf
@@ -5,5 +5,5 @@ data "aws_partition" "current" {}
 data "aws_caller_identity" "current" {}
 
 data "aws_s3_bucket" "firehose_bucket" {
-  bucket = "cms-cloud-${data.aws_caller_identity.current.account_id}-${data.aws_region.current}"
+  bucket = "cms-cloud-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"
 }
