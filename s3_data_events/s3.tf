bb0eebf43e0e43887c66bc41bcbedb8016fabb90
Dustin Whited <dgwhited@gmail.com>
2022-11-18 09:13:22 -0600

    ct

--- s3_data_events/s3.tf
+++ s3_data_events/s3.tf
@@ -78,7 +78,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
   rule {
     bucket_key_enabled = true
     apply_server_side_encryption_by_default {
-      sse_algorithm = "aws:kms"
+      sse_algorithm = "AES256"
     }
   }
 }
