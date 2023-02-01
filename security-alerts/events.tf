5c2b24f7394804bb184f8cb73d034ffa0bf010e4
rileydakota <dakotariley2@gmail.com>
2023-02-01 15:57:29 -0500

    fix: remove old nessus rule

--- security-alerts/events.tf
+++ security-alerts/events.tf
@@ -32,39 +32,6 @@ resource "aws_cloudwatch_event_target" "target" {
   arn       = aws_sns_topic.slack_topic.id
 }
 
-# Security hub nessus only
-resource "aws_cloudwatch_event_rule" "nessus" {
-  name        = "sechub-findings-to-lambda-nessus"
-  description = "Sends Security Hub nessus findings to a Slack Lambda"
-
-  event_pattern = <<EOF
-{
-  "source": [
-    "aws.securityhub"
-  ],
-  "detail-type": [
-    "Security Hub Findings - Imported"
-  ],
-  "detail": {
-    "findings": {
-      "RecordState": ["ACTIVE"],
-      "WorkflowState": ["NEW"],
-      "Severity": {
-        "Label": [ "CRITICAL" ]
-      },
-      "ProductName": [ "Default" ]
-    }
-  }
-}
-EOF
-}
-
-resource "aws_cloudwatch_event_target" "nessus" {
-  rule      = aws_cloudwatch_event_rule.nessus.name
-  target_id = aws_cloudwatch_event_rule.nessus.name
-  arn       = aws_sns_topic.slack_topic.id
-}
-
 # GuardDuty
 resource "aws_cloudwatch_event_rule" "guardduty" {
   name        = "guardduty-findings-to-lambda"
