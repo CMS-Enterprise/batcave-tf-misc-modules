d1c1a384a968cd6e7bf8714b3c77763119ed435a
rileydakota <dakotariley2@gmail.com>
2023-04-07 12:32:34 -0400

    fix: properly index into array, print error without concat

--- security-alerts/code/sechub_transform.py
+++ security-alerts/code/sechub_transform.py
@@ -4,8 +4,9 @@ def handler(event, context):
     
     account_name = os.environ.get("ACCOUNT_NAME")
     try:
-        event['detail']['findings']['AwsAccountId'] = account_name
+        event['detail']['findings'][0]['AwsAccountId'] = account_name
     except Exception as e:
-        print('Error encountered during parsing of event for AwsAccountId: ' + e)
+        print('Error encountered during parsing of event for AwsAccountId')
+        print(e)
         return event
     return event
