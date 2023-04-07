643d3cb93499f5cde86933f095dd802d57ca2de3
rileydakota <dakotariley2@gmail.com>
2023-04-07 12:23:25 -0400

    fix: lambda not updating correct key for AwsAccountId

--- security-alerts/code/sechub_transform.py
+++ security-alerts/code/sechub_transform.py
@@ -3,6 +3,9 @@ import os
 def handler(event, context):
     
     account_name = os.environ.get("ACCOUNT_NAME")
-    event['account'] = account_name
-
+    try:
+        event['detail']['findings']['AwsAccountId'] = account_name
+    except Exception as e:
+        print('Error encountered during parsing of event for AwsAccountId: ' + e)
+        return event
     return event
