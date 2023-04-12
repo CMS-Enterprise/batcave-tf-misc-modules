ed42aa039aa14979d6cee86e2a39233b3f572988
rileydakota <dakotariley2@gmail.com>
2023-04-12 17:48:30 -0400

    fix: add prefix to destination arn

--- security-alerts/guardduty.tf
+++ security-alerts/guardduty.tf
@@ -2,7 +2,7 @@ data "aws_guardduty_detector" "cms_cloud_gd" {}
 
 resource "aws_guardduty_publishing_destination" "s3-export" {
   detector_id     = data.aws_guardduty_detector.cms_cloud_gd.id
-  destination_arn = data.aws_s3_bucket.cms_cloud_logs.arn
+  destination_arn = "${data.aws_s3_bucket.cms_cloud_logs.arn}/guardduty/"
   kms_key_arn     = aws_kms_key.gd_export_kms_key.arn
 }
 
