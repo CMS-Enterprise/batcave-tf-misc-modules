3ea31c20683a22458c1ede865865f7aa8abf01f9
rileydakota <DAKOTARILEY2@GMAIL.COM>
2023-04-14 11:11:28 -0400

    BATSAD-1127: Ingest AWS GuardDuty to S3 (#22)
    * feat: guardduty forward to s3
    
    * feat: add guardduty s3 export to cms cloud logging bucket
    
    * fix: reference correct kms key
    
    * fix: detector and s3 destination are data sources
    
    * fix: include cms required path in role arn
    
    * fix: add prefix to destination arn
    
    * fix: add resource block to kms key arn
    
    * feat: auto created needed prefix
    
    * fix: use aws_s3_object instead

---
+++ security-alerts/guardduty.tf
@@ -0,0 +1,16 @@
+data "aws_guardduty_detector" "cms_cloud_gd" {}
+
+resource "aws_guardduty_publishing_destination" "s3-export" {
+  detector_id     = data.aws_guardduty_detector.cms_cloud_gd.id
+  destination_arn = "${data.aws_s3_bucket.cms_cloud_logs.arn}/guardduty/"
+  kms_key_arn     = aws_kms_key.gd_export_kms_key.arn
+}
+
+data "aws_s3_bucket" "cms_cloud_logs" {
+  bucket = "cms-cloud-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"
+}
+
+resource "aws_s3_object" "guardduty_directory" {
+    bucket  = "${data.aws_s3_bucket.cms_cloud_logs.id}"
+    key     =  "guardduty/"
+}
