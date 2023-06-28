1793d89ae6d1c7e4b9e236fa7936cca9372bc77a
rileydakota <dakotariley2@gmail.com>
2023-06-28 16:33:08 -0400

    refactor gd export in security-alerts

--- sdl_logs/s3.tf
+++ sdl_logs/s3.tf
@@ -23,3 +23,30 @@ resource "null_resource" "bucket_notification" {
 data "external" "bucket_notification" {
   program = ["sh", "-c", "aws s3api get-bucket-notification-configuration --bucket ${data.aws_s3_bucket.cms_logging_bucket.id} --output json | jq -r '.EventBridgeConfiguration |if .==null then {\"eventbridge\":\"false\"} else {\"eventbridge\":\"true\"} end'"]
 }
+
+data "aws_iam_policy_document" "gd_export_s3_bucket" {
+  statement {
+    effect    = "allow"
+    actions   = ["s3:GetBucketLocation"]
+    resources = [aws_s3_bucket.gd_export_s3_bucket.arn]
+    principals {
+      type        = "Service"
+      identifiers = ["guardduty.amazonaws.com"]
+    }
+  }
+
+  statement {
+    effect    = "allow"
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.gd_export_s3_bucket.arn}/*"]
+  }
+}
+
+resource "aws_s3_bucket_policy" "gd_s3_export_bucket" {
+  bucket = aws_s3_bucket.gd_export_s3_bucket.id
+  policy = data.aws_iam_policy_document.gd_export_s3_bucket
+}
+
+resource "aws_s3_bucket" "gd_export_s3_bucket" {
+  bucket = "batcave-gd-s3-export-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"
+}
