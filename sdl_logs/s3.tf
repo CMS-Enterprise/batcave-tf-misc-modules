887fce4fbc44324d03fdddf94fea9b469f9e62a9
rileydakota <dakotariley2@gmail.com>
2023-06-29 11:45:59 -0400

    fix casing

--- sdl_logs/s3.tf
+++ sdl_logs/s3.tf
@@ -36,7 +36,7 @@ data "aws_iam_policy_document" "gd_export_s3_bucket" {
   }
 
   statement {
-    effect    = "allow"
+    effect    = "Allow"
     actions   = ["s3:PutObject"]
     resources = ["${aws_s3_bucket.gd_export_s3_bucket.arn}/*"]
   }
