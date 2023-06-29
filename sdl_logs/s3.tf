7b81af67f677d4459ffc249f26e2c54bf73c19d3
rileydakota <dakotariley2@gmail.com>
2023-06-29 11:42:13 -0400

    add .json to policy reference

--- sdl_logs/s3.tf
+++ sdl_logs/s3.tf
@@ -44,7 +44,7 @@ data "aws_iam_policy_document" "gd_export_s3_bucket" {
 
 resource "aws_s3_bucket_policy" "gd_s3_export_bucket" {
   bucket = aws_s3_bucket.gd_export_s3_bucket.id
-  policy = data.aws_iam_policy_document.gd_export_s3_bucket
+  policy = data.aws_iam_policy_document.gd_export_s3_bucket.json
 }
 
 resource "aws_s3_bucket" "gd_export_s3_bucket" {
