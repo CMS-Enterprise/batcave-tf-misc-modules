c1cb705bc9cb3862faa2f62b85a35a88fc90553c
Hasan Tutac <hasan.tutac@cms.hhs.gov>
2024-06-06 11:44:54 -0400

    removed api service role

--- role/main.tf
+++ role/main.tf
@@ -82,34 +82,34 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_logs_attachment" {
 }
 
 # Existing roles and policies
-resource "aws_iam_role" "api-service-role" {
-  name = "${var.GroupName}-api-service-role"
-  depends_on = [
-      aws_iam_policy.api-policy
-    ]
-  path = var.iam_role_path
-  permissions_boundary = var.permissions_boundary
-  tags = var.tags
-  assume_role_policy = <<-EOF
-  { 
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-          "Action": "sts:AssumeRoleWithWebIdentity",
-          "Effect": "Allow",
-          "Principal" : {
-            "Federated": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/${var.OIDCProviderID}"
-          },
-          "Condition": {
-            "StringEquals": {
-              "oidc.eks.us-east-1.amazonaws.com/id/${var.OIDCProviderID}:aud": "sts.amazonaws.com",
-              "oidc.eks.us-east-1.amazonaws.com/id/${var.OIDCProviderID}:sub": "system:serviceaccount:${var.NameSpace != null ? var.NameSpace : var.GroupName}:${var.GroupName}-api-service-account"
-              }
-          }
-      }]
-    }
-      EOF
-}
+// resource "aws_iam_role" "api-service-role" {
+//   name = "${var.GroupName}-api-service-role"
+//   depends_on = [
+//       aws_iam_policy.api-policy
+//     ]
+//   path = var.iam_role_path
+//   permissions_boundary = var.permissions_boundary
+//   tags = var.tags
+//   assume_role_policy = <<-EOF
+//   { 
+//     "Version": "2012-10-17",
+//     "Statement": [
+//       {
+//           "Action": "sts:AssumeRoleWithWebIdentity",
+//           "Effect": "Allow",
+//           "Principal" : {
+//             "Federated": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/${var.OIDCProviderID}"
+//           },
+//           "Condition": {
+//             "StringEquals": {
+//               "oidc.eks.us-east-1.amazonaws.com/id/${var.OIDCProviderID}:aud": "sts.amazonaws.com",
+//               "oidc.eks.us-east-1.amazonaws.com/id/${var.OIDCProviderID}:sub": "system:serviceaccount:${var.NameSpace != null ? var.NameSpace : var.GroupName}:${var.GroupName}-api-service-account"
+//               }
+//           }
+//       }]
+//     }
+//       EOF
+// }
 
 // resource "aws_iam_role" "job-scheduler-service-role" {
 //   name = "${var.GroupName}-job-scheduler-service-role"
@@ -172,35 +172,35 @@ resource "aws_iam_role" "api-service-role" {
 //   EOF
 // }
 
-resource "aws_iam_policy" "api-policy" {
-  name        = "${var.GroupName}-api-policy"
-  path        = "/delegatedadmin/developer/"
-  tags        = var.tags
+// resource "aws_iam_policy" "api-policy" {
+//   name        = "${var.GroupName}-api-policy"
+//   path        = "/delegatedadmin/developer/"
+//   tags        = var.tags
 
-  policy = <<-EOF
-  {
-    "Version": "2012-10-17",
-    "Statement":
-    [
-      {
-        "Effect": "Allow",
-          "Action": [
-            "s3:PutObject",
-            "s3:GetObject",
-            "s3:DeleteObject",
-            "s3:ListBucket",
-            "sqs:GetQueueUrl",
-            "sqs:DeleteMessage",
-            "sqs:ReceiveMessage",
-            "sqs:SendMessage",
-            "sqs:GetQueueAttributes"
-            ],
-          "Resource": ${local.ApiResources}
-      }
-    ]
-  }
-  EOF
-}
+//   policy = <<-EOF
+//   {
+//     "Version": "2012-10-17",
+//     "Statement":
+//     [
+//       {
+//         "Effect": "Allow",
+//           "Action": [
+//             "s3:PutObject",
+//             "s3:GetObject",
+//             "s3:DeleteObject",
+//             "s3:ListBucket",
+//             "sqs:GetQueueUrl",
+//             "sqs:DeleteMessage",
+//             "sqs:ReceiveMessage",
+//             "sqs:SendMessage",
+//             "sqs:GetQueueAttributes"
+//             ],
+//           "Resource": ${local.ApiResources}
+//       }
+//     ]
+//   }
+//   EOF
+// }
 
 // locals {
 //   ApiResources = "[\"${join("\",\"",var.ApiResources)}\"]"
@@ -263,10 +263,10 @@ resource "aws_iam_policy" "api-policy" {
 //   policy_arn = aws_iam_policy.snowflake-access-policy.arn
 // }
 
-resource "aws_iam_role_policy_attachment" "api_policy" {
-  role       = aws_iam_role.api-service-role.name
-  policy_arn = aws_iam_policy.api-policy.arn
-}
+// resource "aws_iam_role_policy_attachment" "api_policy" {
+//   role       = aws_iam_role.api-service-role.name
+//   policy_arn = aws_iam_policy.api-policy.arn
+// }
 
 // resource "aws_iam_role_policy_attachment" "job_scheduler_policy" {
 //   role       = aws_iam_role.job-scheduler-service-role.name
